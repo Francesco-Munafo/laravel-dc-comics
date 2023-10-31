a294bfc3debc6acd9d06e92a641821ce5e2fbdc0
Francesco-Munafo <acquamarea1@gmail.com>
2023-10-31 13:56:09 +0100

    Add MSCR Structure

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        // This method simplify seeder population 
+        $this->call([
+            ComicSeeder::class
+        ]);
     }
 }
